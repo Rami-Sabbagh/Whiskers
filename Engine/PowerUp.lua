10ff82585a00445f2830a61849a1efd3a7ce9a49
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-01 17:54:29 +0200

    WIP PowerUps

---
+++ Engine/PowerUp.lua
@@ -0,0 +1,65 @@
+local class = require("Libraries.middleclass")
+
+local Resources = require("Engine.Resources")
+
+local PowerUp = class("PowerUp")
+
+PowerUp.types = {
+  "bomb",
+  "bullet",
+  "lightning",
+  "star"
+}
+
+function PowerUp:initialize( game, x, y, id )
+  
+  self.game = game
+  
+  self.world = self.game.world
+  self.worldWidth, self.worldHeight = self.game.worldWidth, self.game.worldHeight
+  
+  self.id = id or love.math.random(1,#self.types)
+  self.name = self.types[self.id]
+  
+  self.size = self.game.PTM/2
+  
+  self.image = Resources.Image[self.name.."Icon"]
+  self.imageWidth, self.imageHeight = self.image:getDimensions()
+  self.imageScale = self.size/self.imageWidth
+  
+  self.imageOX, self.imageOY = self.imageWidth/2, self.imageHeight/2
+  
+  self.body = love.physics.newBody(self.world, x, y, "dynamic")
+  self.shape = love.physics.newRectangleShape(self.size,self.size)
+  self.fixture = love.physics.newFixture(self.body, self.shape)
+  
+  self.body:setUserData(self)
+end
+
+function PowerUp:draw()
+  local bx, by = self.body:getPosition()
+  local rot = self.body:getAngle()
+  
+  love.graphics.setColor(255,255,255,255)
+  love.graphics.draw(self.image, bx,by, rot, self.imageScale,self.imageScale, self.imageOX,self.imageOY)
+end
+
+function PowerUp:update(dt)
+  
+  
+  local bx, by = self.body:getPosition()
+  if bx < 0 then
+    self.body:setX(self.worldWidth)
+  elseif bx > self.worldWidth then
+    self.body:setX(0)
+  end
+  
+  if by < 0 then
+    self.body:setY(self.worldHeight)
+  elseif by > self.worldHeight then
+    self.body:setY(0)
+  end
+  
+end
+
+return PowerUp
