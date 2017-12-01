7fcc148c9b5518aff03f4cf205137ff8d9f0f41d
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-01 11:41:27 +0200

    Added a function to play random meow sounds

--- Engine/Resources.lua
+++ Engine/Resources.lua
@@ -6,4 +6,25 @@ R.Image = {}
 R.Music = {}
 R.SFX = {}
 
+R.meowNames = {}
+
+for i = 1, 10 do
+
+  local filename
+  
+  if i < 10 then
+    filename = string.format("meow-0%d",i)
+  else
+    filename = string.format("meow-%d",i)
+  end
+  
+  table.insert(R.meowNames, filename)
+  
+end
+
+function R:playMeow()
+  local rnd = love.math.random(1,#self.meowNames)
+  self.SFX[self.meowNames[rnd]]:play()
+end
+
 return R
