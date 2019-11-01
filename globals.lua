c42b289ed1ed8c0b3ad256bba617c6b25244ef93
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-11-01 19:36:20 +0200

    New `_colorPallet` global

--- globals.lua
+++ globals.lua
@@ -13,5 +13,14 @@ _image = {} --Contains all the loaded images
 _sfx = {} --Contains all the loaded sfx
 _music = {} --Contains all the loaded music
 
-_meowNames = {} --Contans the name of meow sfx
+_meowNames = {} --Contains the name of meow sfx
 for i = 1, 10 do _meowNames[i] = (i < 10) and "meow-0"..i or "meow-"..i end
+
+--The color palette of the game
+_colorPalette = {
+	{96/255 , 246/255, 133/255, 1}, --Green
+	{246/255, 207/255, 95/255 , 1}, --Yellow
+	{95/255 , 134/255, 246/255, 1}, --Blue
+	{246/255, 95/255 , 209/255, 1}, --Pink
+	{246/255, 132/255, 96/255 , 1}  --Orange
+}
