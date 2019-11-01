4339c02fef8665ebb33e60afc81cd4858331e729
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-11-01 08:59:15 +0200

    Cleanup splash state

--- states/splash.lua
+++ states/splash.lua
@@ -1,171 +1,143 @@
 local loader = require("libraries.love-loader")
 local gamestate = require("libraries.gamestate")
-local resources = require("engine.resources")
+local resources = require("whiskers.resources")
 
-local SState = {}
+local splashState = {}
 
-local SWidth, SHeight = love.graphics.getDimensions()
+local screenWidth, screenHeight = love.graphics.getDimensions()
 
-function SState:init()
-  --Load the RL4G Logo
-  self.logoImage = love.graphics.newImage("assets/miscellaneous/RL4G_LOGO.png")
-  self.logoX = math.floor((SWidth - self.logoImage:getWidth())/2)
-  self.logoY = math.floor((SHeight - self.logoImage:getHeight())/2)
-  
-  --Add resources to load
-  self:AddImages()
-  self:AddSFX()
-  self:AddMusic()
+function splashState:init()
+	--Load the RL4G Logo
+	self.logoImage = love.graphics.newImage("assets/miscellaneous/RL4G_LOGO.png")
+	self.logoX = math.floor((screenWidth - self.logoImage:getWidth())/2)
+	self.logoY = math.floor((screenHeight - self.logoImage:getHeight())/2)
+	
+	--Add resources to load
+	self:queueImages()
+	self:queueSFX()
+	self:queueMusic()
 end
 
-function SState:enter()
-  
-  print("--==Entered Spash State==--")
-  
-  love.graphics.setBackgroundColor(70/255,70/255,70/255,1)
-  
-  self.finishedLoading = false
-  
-  loader.start(
-    
-    --Finished Loading
-    function()
-      print("Finished Loading")
-      self.finishedLoading = true
-      gamestate.switch( require("States.Game") )
-    end,
-    
-    --Item Loaded
-    function(kind, holder, key)
-      print("Loaded", math.floor((loader.loadedCount/loader.resourceCount)*100) .. "%",kind, key)
-      
-      if kind == "stream" then
-        holder[key]:setLooping(true)
-        holder[key]:setVolume(0.9)
-      end
-    end
-    
-    )
-  
-  print("Loader Started")
-  
-  print("Resources to load:", loader.resourceCount)
-  
+function splashState:enter()
+	print("--==Entered Spash State==--")
+	
+	love.graphics.setBackgroundColor(70/255,70/255,70/255,1)
+	
+	self.finishedLoading = false
+	
+	loader.start(
+		--Finished Loading
+		function()
+			print("Finished Loading")
+			self.finishedLoading = true
+			gamestate.switch( require("states.game") )
+		end,
+		
+		--Item Loaded
+		function(kind, holder, key)
+			print("Loaded", math.floor((loader.loadedCount/loader.resourceCount)*100) .. "%",kind, key)
+			
+			if kind == "stream" then
+				holder[key]:setLooping(true)
+				holder[key]:setVolume(0.9)
+			end
+		end
+	)
+	
+	print("Loader Started")
+	print("Resources to load:", loader.resourceCount)
 end
 
-function SState:leave()
-  
+function splashState:draw()
+	--Draw RamiLego4Game's logo
+	love.graphics.setColor(1,1,1,1)
+	love.graphics.draw(self.logoImage, self.logoX, self.logoY)
 end
 
-function SState:draw()
-  
-  love.graphics.setColor(1,1,1,1)
-  love.graphics.draw(self.logoImage, self.logoX, self.logoY)
-  
+function splashState:update(dt)
+	--Update the loader
+	if not self.finishedLoading then
+		loader.update(dt)
+	end
 end
 
-function SState:update(dt)
-  
-  if not self.finishedLoading then
-    loader.update(dt)
-  end
-  
+function splashState:queueImages(path)
+	local path = path or "/assets/images/"
+	local items = love.filesystem.getDirectoryItems(path)
+	
+	for id, item in ipairs(items) do
+		local itemPath = path..item
+		
+		if love.filesystem.getInfo(itemPath, "directory") then
+			self:queueImages(itemPath.."/")
+		else
+			local _, fileName, fileExtension = self:splitFilePath(itemPath)
+			if fileExtension == "png" then
+				loader.newImage(resources.Image, fileName, itemPath)
+			end
+		end
+	end
 end
 
-function SState:keypressed(key, scancode, isRepeat)
-  
+function splashState:queueSFX(path)
+	local path = path or "/assets/sfx/"
+	local items = love.filesystem.getDirectoryItems(path)
+	
+	for id, item in ipairs(items) do
+		
+		local itemPath = path..item
+		
+		if love.filesystem.getInfo(itemPath, "directory") then
+			
+			self:queueSFX(itemPath.."/")
+			
+		else
+			
+			local _, fileName, fileExtension = self:splitFilePath(itemPath)
+			
+			if fileExtension == "wav" then
+				
+				loader.newSource(resources.SFX, fileName, itemPath)
+				
+			end
+			
+		end
+		
+	end
 end
 
-function SState:touchpressed(id, x,y, dx,dy, pressure)
-  
-end
-
-function SState:AddImages(path)
-  local path = path or "/Assets/Images/"
-  local Items = love.filesystem.getDirectoryItems(path)
-  
-  for id, item in ipairs(Items) do
-    
-    local ItemPath = path..item
-    
-    if love.filesystem.getInfo(ItemPath,"directory") then
-      
-      self:AddImages(ItemPath.."/")
-      
-    else
-      
-      local _, FileName, FileExtension = self:SplitFilePath(ItemPath)
-      
-      if FileExtension == "png" then
-        
-        loader.newImage(resources.Image, FileName, ItemPath)
-        
-      end
-      
-    end
-    
-  end
-end
-
-function SState:AddSFX(path)
-  local path = path or "/Assets/SFX/"
-  local Items = love.filesystem.getDirectoryItems(path)
-  
-  for id, item in ipairs(Items) do
-    
-    local ItemPath = path..item
-    
-    if love.filesystem.getInfo(ItemPath,"directory") then
-      
-      self:AddImages(ItemPath.."/")
-      
-    else
-      
-      local _, FileName, FileExtension = self:SplitFilePath(ItemPath)
-      
-      if FileExtension == "wav" then
-        
-        loader.newSource(resources.SFX, FileName, ItemPath)
-        
-      end
-      
-    end
-    
-  end
-end
-
-function SState:AddMusic(path)
-  local path = path or "/Assets/Music/"
-  local Items = love.filesystem.getDirectoryItems(path)
-  
-  for id, item in ipairs(Items) do
-    
-    local ItemPath = path..item
-    
-    if love.filesystem.getInfo(ItemPath,"directory") then
-      
-      self:AddImages(ItemPath.."/")
-      
-    else
-      
-      local _, FileName, FileExtension = self:SplitFilePath(ItemPath)
-      
-      if FileExtension == "mp3" then
-        
-        loader.newSource(resources.Music, FileName, ItemPath, "stream")
-        
-      end
-      
-    end
-    
-  end
+function splashState:queueMusic(path)
+	local path = path or "/assets/music/"
+	local items = love.filesystem.getDirectoryItems(path)
+	
+	for id, item in ipairs(items) do
+		
+		local itemPath = path..item
+		
+		if love.filesystem.getInfo(itemPath, "directory") then
+			
+			self:queueMusic(itemPath.."/")
+			
+		else
+			
+			local _, fileName, fileExtension = self:splitFilePath(ItemPath)
+			
+			if fileExtension == "mp3" then
+				
+				loader.newSource(resources.Music, fileName, itemPath, "stream")
+				
+			end
+			
+		end
+		
+	end
 end
 
 --Extra functions
 
-function SState:SplitFilePath(path)
-  local p, n, e = path:match("(.-)([^\\/]-%.?([^%.\\/]*))$")
-  return p, n:sub(1, -e:len()-2), e
+function splashState:splitFilePath(path)
+	local p, n, e = path:match("(.-)([^\\/]-%.?([^%.\\/]*))$")
+	return p, n:sub(1, -e:len()-2), e
 end
 
-return SState
+return splashState
