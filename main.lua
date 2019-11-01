3b89c8d7e9874d678d30afdd1ba1abc8142dd43b
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-11-01 08:35:56 +0200

    Case changes

--- main.lua
+++ main.lua
@@ -1,25 +1,25 @@
-local GameState = require("Libraries.gamestate")
-local lovebird = require("Libraries.lovebird")
+local gamestate = require("libraries.gamestate")
+local lovebird = require("libraries.lovebird")
 
-local SplashState = require("States.Splash")
+local splashState = require("states.splash")
 
 function love.load(args)
-  if args[#args] == "-debug" then
-    _DEBUG = true
-  end
-  
-  if _DEBUG then lovebird.init() end
-  
-  GameState.registerEvents()
-  GameState.switch(SplashState)
+	if args[#args] == "-debug" then
+		_DEBUG = true
+	end
+	
+	if _DEBUG then lovebird.init() end
+	
+	gamestate.registerEvents()
+	gamestate.switch(splashstate)
 end
 
 function love.update(dt)
-  if _DEBUG then lovebird.update(dt) end
+	if _DEBUG then lovebird.update(dt) end
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  if key == "escape" then
-    love.event.quit()
-  end
+	if key == "escape" then
+		love.event.quit()
+	end
 end
